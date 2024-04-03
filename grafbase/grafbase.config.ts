b1d9cc4ccc358f10d38220b0da6ce642f17bfd8f
youngDevelopers <dambaemmanuel5@gmail.com>
2024-04-03 08:56:35 +0200

    Grafbase Schemas

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,30 +1,42 @@
-import { graph, config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk';
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
+})
 
-const g = graph.Standalone()
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+})
 
-// Data Sources - https://grafbase.com/docs/connectors
-//
-// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
-// g.datasource(pg)
-
-// Resolvers - https://grafbase.com/docs/resolvers
-//
-// g.query('helloWorld', {
-//   returns: g.string(),
-//   resolver: 'hello-world',
-// })
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
+})
 
 export default config({
-  graph: g,
-  // Authentication - https://grafbase.com/docs/auth
+  schema: g,
   auth: {
-    rules: (rules) => {
-      rules.public()
-    },
+    providers: [jwt],
+    rules: (rules) => rules.private()
   },
-
 })
+
